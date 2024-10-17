60a16140804cdc9aa56019b513ecbc35e837a3e7
kikecastillocolombia <enriquecastiyosoy@gmail.com>
2024-10-16 22:47:50 -0500

    crear mesa estilizado

--- frontend/src/app/mesas/page.tsx
+++ frontend/src/app/mesas/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import { getMesas, createMesa } from "./mesas.api"; // Asegúrate de importar createMesa
 import { MesaCard } from "@/components/mesa-card"; 
--- frontend/src/components/ui/CrearMesaForm.tsx
+++ frontend/src/components/ui/CrearMesaForm.tsx
@@ -1,10 +1,11 @@
-"use client";
-import { useState } from "react";
-import { createMesa } from "@/app/mesas/mesas.api"; // Asegúrate de que esto apunta a la ruta correcta
-import { MesaWithoutId } from "@/types"; // Asegúrate de que la ruta sea correcta
-
+"use client"
+import React, { useState } from "react";
+import { createMesa } from "@/app/mesas/mesas.api"; 
+import { MesaWithoutId } from "@/types"; 
+import { buttonVariants } from "./button";
+import { Input } from "./input";
 interface CrearMesaFormProps {
-  onNewMesa: (mesaData: MesaWithoutId) => void; // Cambiado a MesaWithoutId
+  onNewMesa: (mesaData: MesaWithoutId) => void;
 }
 
 export default function CrearMesaForm({ onNewMesa }: CrearMesaFormProps) {
@@ -12,33 +13,32 @@ export default function CrearMesaForm({ onNewMesa }: CrearMesaFormProps) {
 
   async function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
-
     const numeroParsed = parseInt(numero, 10);
     if (isNaN(numeroParsed)) {
       alert("Por favor, ingresa un número válido para la mesa.");
       return;
     }
-
-    const newMesa = await createMesa({ numero: numeroParsed }); // Guardar el nuevo objeto creado
+    const newMesa = await createMesa({ numero: numeroParsed });
     console.log(newMesa);
     
-    setNumero(''); // Limpiar el campo después de la creación
-    onNewMesa({ numero: numeroParsed }); // Llama al callback
+    setNumero('');
+    onNewMesa({ numero: numeroParsed });
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
       <div>
         <label htmlFor="numero">Número de Mesa</label>
-        <input
+        <Input
           id="numero"
           type="number"
           value={numero}
           onChange={(e) => setNumero(e.target.value)}
           required
+          className="border-2 border-gray-300 rounded-md p-2"
         />
       </div>
-      <button type="submit">
+      <button className={buttonVariants()} type="submit">
         Crear Mesa
       </button>
     </form>
